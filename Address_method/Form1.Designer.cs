faf7e8777bce8e90b80c85ae721464c91d3dd24e
Michael Biggs <mikeylfc007@blueyonder.co.uk>
2015-01-26 20:16:00 +0000

    Added full app

---
+++ Address_method/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace Address_method
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.txtAddressResult = new System.Windows.Forms.TextBox();
+            this.txtNoLines = new System.Windows.Forms.TextBox();
+            this.txtDirect = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblText = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(436, 176);
+            this.btnStart.Margin = new System.Windows.Forms.Padding(4);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(100, 28);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Test";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(13, 62);
+            this.txtAddress.Margin = new System.Windows.Forms.Padding(4);
+            this.txtAddress.Multiline = true;
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(403, 111);
+            this.txtAddress.TabIndex = 2;
+            // 
+            // txtAddressResult
+            // 
+            this.txtAddressResult.Location = new System.Drawing.Point(436, 62);
+            this.txtAddressResult.Margin = new System.Windows.Forms.Padding(4);
+            this.txtAddressResult.Multiline = true;
+            this.txtAddressResult.Name = "txtAddressResult";
+            this.txtAddressResult.Size = new System.Drawing.Size(403, 111);
+            this.txtAddressResult.TabIndex = 2;
+            // 
+            // txtNoLines
+            // 
+            this.txtNoLines.Location = new System.Drawing.Point(740, 179);
+            this.txtNoLines.Name = "txtNoLines";
+            this.txtNoLines.Size = new System.Drawing.Size(100, 22);
+            this.txtNoLines.TabIndex = 3;
+            // 
+            // txtDirect
+            // 
+            this.txtDirect.Location = new System.Drawing.Point(165, 179);
+            this.txtDirect.Name = "txtDirect";
+            this.txtDirect.Size = new System.Drawing.Size(251, 22);
+            this.txtDirect.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(666, 182);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "No. Lines";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(38, 182);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(121, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Directory Address";
+            // 
+            // lblText
+            // 
+            this.lblText.AutoSize = true;
+            this.lblText.Location = new System.Drawing.Point(12, 9);
+            this.lblText.Name = "lblText";
+            this.lblText.Size = new System.Drawing.Size(0, 17);
+            this.lblText.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(852, 217);
+            this.Controls.Add(this.lblText);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtDirect);
+            this.Controls.Add(this.txtNoLines);
+            this.Controls.Add(this.txtAddressResult);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.btnStart);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Address Method Tester";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.TextBox txtAddressResult;
+        private System.Windows.Forms.TextBox txtNoLines;
+        private System.Windows.Forms.TextBox txtDirect;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblText;
+    }
+}
+
---
+++ Address_method/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Address_method
+{
+    public partial class Form1 : Form
+    {
+        int width = 350 / 20;
+        int numberOfLines = 0;
+        StringBuilder sb = new StringBuilder();
+        string textrow;
+        string InitialDirectory;
+        List<string> addressItems = new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            txtAddress.Clear();
+            txtAddressResult.Clear();
+            try
+            {
+                numberOfLines = Convert.ToInt32(txtNoLines.Text);
+                InitialDirectory = txtDirect.Text;
+                lblText.Text = txtDirect.Text;
+            }
+            catch
+            {
+                numberOfLines = 4;
+
+                FolderBrowserDialog FB = new FolderBrowserDialog();
+                if (FB.ShowDialog() == DialogResult.OK)
+                {
+                    InitialDirectory = FB.SelectedPath;
+                }
+                else
+                    return;
+                lblText.Text = "Default";
+            }
+           
+            
+            var files = new DirectoryInfo(InitialDirectory).GetFiles();
+            int index = new Random().Next(0, files.Length);
+            FileStream fs1 = new FileStream(InitialDirectory +'\\' + files[index].Name, FileMode.Open);
+            StreamReader sr1 = new StreamReader(fs1);
+
+            while ((textrow = sr1.ReadLine()) != null)
+            {
+                string[] s = textrow.Split(',');
+                foreach (string add in s)
+                {
+                    addressItems.Add(add);
+                }   
+            }
+            sr1.Close();
+            fs1.Close();
+        
+                   
+            //Before
+            foreach (string prime in addressItems) // Loop through List with foreach
+            {
+                txtAddress.AppendText(prime + "\n");
+            }
+            //Method doing its work
+            AddressLimit(addressItems, width, numberOfLines);
+            //After
+            foreach (string prime in addressItems) // Loop through List with foreach
+            {
+                txtAddressResult.AppendText(prime + "\n");
+            }
+            this.Refresh();
+            addressItems.Clear();
+            sb.Clear();
+        }
+
+        public void AddressLimit(List<string> addressItems, int width, int numberOfLines)
+        {
+            string Address;
+            int CurrentCapacity;
+            //Builds List into a String
+            StringBuilder sb = new StringBuilder();
+            foreach (string address in addressItems)
+            {
+                sb.Append(address).Append(",");
+            }
+            addressItems.Clear();
+            //Splits the string into a array
+            string[] split = sb.ToString().Split(',');
+            //string[] split = addressItems.ToArray()            
+            //Shortens the line depending on whether it meets the char limit or not
+            foreach (string address in split)
+            {
+                if (width < address.Length)
+                {
+                    Address = address.Substring(0, width - 3) + "...";
+                    addressItems.Add(Address);
+                }
+                else if (String.IsNullOrWhiteSpace(address))
+                {
+                }
+                else
+                {
+                    Address = address;
+                    addressItems.Add(Address);
+                }
+            }
+            CurrentCapacity = addressItems.Count;
+            ///////////
+            //Checks to see if lines can be saved an joined together
+            for (int i = 0; i < CurrentCapacity; i++)
+            {
+                //Saving line sapce
+                if ((i + 1 < CurrentCapacity) && (addressItems[i].Length + addressItems[i + 1].Length <= width - 1))
+                {
+                    addressItems[i] = addressItems[i] + "," + addressItems[i + 1];
+                    addressItems.Remove(addressItems[i + 1]);
+                    CurrentCapacity = addressItems.Count;
+                }
+            }
+            ///////////
+            //if any lines are left on that dont meet the required line count
+            //it just throws it off(Bar the last line(usally contains the PC), it removes the 2nd to last line)
+            for (int i = numberOfLines; i < CurrentCapacity; )
+            {
+                addressItems.Remove(addressItems[CurrentCapacity - 2]);
+                CurrentCapacity = addressItems.Count;
+            }
+        }
+    }
+}
+
+
